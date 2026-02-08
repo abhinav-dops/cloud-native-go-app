cf29c793c11b2bbc80e02ceabeb9d8f136acff35
abhinav-dops <abhinavsingh.ag@gmail.com>
2026-02-08 14:58:50 +0530

    initialize HTTP server and register item routes

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"cloud-app-api/internal/handler"
+	"cloud-app-api/internal/repository"
+	"cloud-app-api/internal/service"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	repo := repository.NewMemoryRepo()
+	svc := service.NewItemService(repo)
+	h := handler.NewItemHandler(svc)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/items", h.Items)
+
+	log.Println("Server running on :8080")
+	log.Fatal(http.ListenAndServe(":8080", mux))
+}
